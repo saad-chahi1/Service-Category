3d0367eea5b464ab0e740bccb45c002a4f539b28
SAAD CHAHI <saad.chahi@edu.uca.ma>
2021-07-12 12:51:37 +0200

    first commit

---
+++ src/main/java/org/sid/Security/JWTAuthorizationFilter.java
@@ -0,0 +1,19 @@
+package org.sid.Security;
+
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JWTAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String jwt = request.getHeader("Authorization");
+        if(jwt==null) throw new RuntimeException("Not Authorize");
+        filterChain.doFilter(request, response);
+    }
+}
